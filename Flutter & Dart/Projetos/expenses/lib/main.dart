d2110d3cebb1cd79b9b7f2741207a955b6c68969
Anderson Silva <andersonpsilva@gmail.com>
2021-03-13 19:56:19 -0300

    Aula 13-03 (Despesas Pessoais)

---
+++ Flutter & Dart/Projetos/expenses/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Despessas Pessoais'),
+        ),
+        body: Center(child: Text('Versão Inicial')));
+  }
+}
