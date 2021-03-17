9f1535bb836bd5fac6c318d3b0b9a52742e3bede
Anderson Silva <andersonpsilva@gmail.com>
2021-03-16 22:08:43 -0300

    Aula 16-03

--- Flutter & Dart/Projetos/expenses/lib/main.dart
+++ Flutter & Dart/Projetos/expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -11,6 +12,9 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
     Transaction(
       id: 't1',
@@ -33,7 +37,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despessas Pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -55,23 +58,64 @@ class MyHomePage extends StatelessWidget {
                               horizontal: 15, vertical: 10),
                           decoration: BoxDecoration(
                               border: Border.all(
-                            color: Colors.black,
+                            color: Colors.purple,
                             width: 2,
                           )),
                           padding: EdgeInsets.all(10),
-                          child: Text(tr.value.toString())),
+                          child: Text('R\$ ${tr.value.toStringAsFixed(2)}',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple,
+                              ))),
                       Column(
-                        children: [
-                          Text(tr.title),
-                          Text(tr.date.toString()),
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(tr.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.purple,
+                              )),
+                          Text(DateFormat('d MMM y').format(tr.date),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.grey,
+                              )),
                         ],
                       )
                     ],
                   ),
                 );
-              })
+              }),
             ],
-          )
+          ),
+          Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    TextField(
+                        controller: valueController,
+                        decoration: InputDecoration(
+                          labelText: 'Valor (R\$)',
+                        )),
+                    FlatButton(
+                      child: Text('Nova Transação'),
+                      textColor: Colors.purple,
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              )),
         ],
       ),
     );
