bce5addc78e8cb6a3045e30eef92569bea78e6e5
Anderson Silva <andersonpsilva@gmail.com>
2021-05-28 20:51:24 -0300

    Aula 28-05

--- Flutter & Dart/Projetos/Shop/shop/lib/providers/auth.dart
+++ Flutter & Dart/Projetos/Shop/shop/lib/providers/auth.dart
@@ -5,20 +5,33 @@ import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
   static const _apiKey = 'AIzaSyAacFcQ8uM1dpHen5t_Hi_dJ1EU8JaKiNU';
-  static const _signUpWithEmailAndPasswordUrl =
-      'identitytoolkit.googleapis.com';
+  static const _url = 'identitytoolkit.googleapis.com';
   static const _signUpWithEmailAndPasswordPath = '/v1/accounts:signUp';
+  static const _signInWithCustomToken = '/v1/accounts:signIn';
   static const _params = {'key': _apiKey};
 
   Future<void> signup(String email, String password) async {
     final response = await http.post(
-        Uri.https(_signUpWithEmailAndPasswordUrl,
-            _signUpWithEmailAndPasswordPath, _params),
+        Uri.https(_url, _signUpWithEmailAndPasswordPath, _params),
         body: json.encode({
           "email": email,
           'password': password,
           'returnSecureToken': true,
         }));
+
+    print(json.decode(response.body));
+
+    return Future.value();
+  }
+
+  Future<void> login(String email, String password) async {
+    final response =
+        await http.post(Uri.https(_url, _signInWithCustomToken, _params),
+            body: json.encode({
+              "email": email,
+              'password': password,
+              'returnSecureToken': true,
+            }));
 
     print(json.decode(response.body));
 
