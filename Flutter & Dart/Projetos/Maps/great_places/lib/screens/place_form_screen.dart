ad6a7726adf1bcc94b4fab870cd58510e5055e9e
Anderson Silva <77999789+andersonpsilvaa@users.noreply.github.com>
2021-06-29 22:55:38 -0300

    Aula 29-06

--- Flutter & Dart/Projetos/Maps/great_places/lib/screens/place_form_screen.dart
+++ Flutter & Dart/Projetos/Maps/great_places/lib/screens/place_form_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/widgets/image_input.dart';
+import 'package:great_places/widgets/location_input.dart';
 import 'package:provider/provider.dart';
 
 class PlaceFormScreen extends StatefulWidget {
@@ -13,19 +15,33 @@ class PlaceFormScreen extends StatefulWidget {
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
   final _titleController = TextEditingController();
   File _pickedImage;
+  LatLng _pickedPosition;
 
   void _selectImage(File pickedImage) {
-    _pickedImage = pickedImage;
+    setState(() {
+      _pickedImage = pickedImage;
+    });
+  }
+
+  void _selectPosition(LatLng position) {
+    setState(() {
+      _pickedPosition = position;
+    });
+  }
+
+  bool _isValidForm() {
+    return _titleController.text.isNotEmpty &&
+        _pickedImage != null &&
+        _pickedPosition != null;
   }
 
   void _submitForm() {
-    if (_titleController.text.isEmpty || _pickedImage == null) {
-      return;
-    }
+    if (!_isValidForm()) return;
 
     Provider.of<GreatPlaces>(context, listen: false).addPlace(
       _titleController.text,
       _pickedImage,
+      _pickedPosition,
     );
 
     Navigator.of(context).pop();
@@ -43,7 +59,7 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
           Expanded(
             child: SingleChildScrollView(
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(10.0),
                 child: Column(
                   children: [
                     TextField(
@@ -52,8 +68,10 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
                         labelText: 'Título',
                       ),
                     ),
-                    SizedBox(height: 5),
+                    SizedBox(height: 10),
                     ImageInput(this._selectImage),
+                    SizedBox(height: 10),
+                    LocationInput(this._selectPosition),
                   ],
                 ),
               ),
@@ -76,7 +94,7 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
               elevation: 0,
               tapTargetSize: MaterialTapTargetSize.shrinkWrap,
             ),
-            onPressed: _submitForm,
+            onPressed: _isValidForm() ? _submitForm : null,
           ),
         ],
       ),
