6776af82caa48ddc365ad57ab016dc91520e3027
Anderson Silva <77999789+andersonpsilvaa@users.noreply.github.com>
2021-08-02 22:16:29 -0300

    Update chat_screen.dart

--- Flutter & Dart/Projetos/chat/lib/screens/chat_screen.dart
+++ Flutter & Dart/Projetos/chat/lib/screens/chat_screen.dart
@@ -1,9 +1,23 @@
 import 'package:chat/widgets/messages.dart';
 import 'package:chat/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
