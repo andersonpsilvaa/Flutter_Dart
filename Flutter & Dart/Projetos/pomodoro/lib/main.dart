19ebda78a1c62639c05dafcafcb3905805d07041
Anderson Silva <77999789+andersonpsilvaa@users.noreply.github.com>
2022-01-07 22:02:11 -0300

    Aula 07.01.22

--- Flutter & Dart/Projetos/pomodoro/lib/main.dart
+++ Flutter & Dart/Projetos/pomodoro/lib/main.dart
@@ -1,18 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro/pages/Pomodoro.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider<PomodoroStore>(
+          create: (_) => PomodoroStore(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Pomodoro(),
       ),
-      home: Pomodoro(),
     );
   }
 }
+
+// import 'package:flutter_mobx/flutter_mobx.dart';
+// import 'package:pomodoro/store/contador.store.dart';
+// final store = ContadorStore();
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Contador'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Observer(
+//               builder: (_) => Text(
+//                 '${store.contador}',
+//                 style: Theme.of(context).textTheme.headline4,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: store.incrementar,
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
